2bc32f5bb5a6e8d8ef1c01397d0b0318755e8360
Petr Michal <petrmichal0@gmail.com>
2024-08-30 22:49:14 +0200

    Logging Users In

--- screens/LoginScreen.tsx
+++ screens/LoginScreen.tsx
@@ -1,7 +1,27 @@
-import AuthContent from '../components/Auth/AuthContent';
+import { useState } from "react";
+import AuthContent from "../components/Auth/AuthContent";
+import { login } from "../util/auth";
+import LoadingOverlay from "../components/ui/LoadingOverlay";
+
+type loginHandlerProps = {
+  email: string;
+  password: string;
+};
 
 function LoginScreen() {
-  return <AuthContent isLogin />;
+  const [isAuthenticating, setIsAuthenticating] = useState(false);
+
+  async function loginHandler({ email, password }: loginHandlerProps) {
+    setIsAuthenticating(true);
+    await login(email, password);
+    setIsAuthenticating(false);
+  }
+
+  if (isAuthenticating) {
+    return <LoadingOverlay message="Logging you in..." />;
+  }
+
+  return <AuthContent onAuthenticate={loginHandler} isLogin={true} />;
 }
 
 export default LoginScreen;
--- util/auth.ts
+++ util/auth.ts
@@ -2,24 +2,22 @@ import axios from "axios";
 
 const API_KEY = "AIzaSyA9UO7vG-k9XSAD2Ws35U89ey6UuhHbQ6k";
 
+async function authenticate(mode: string, email: string, password: string) {
+  const url = `https://identitytoolkit.googleapis.com/v1/accounts:${mode}?key=${API_KEY}`;
+
+  const response = await axios.post(url, {
+    email: email,
+    password: password,
+    returnSecureToken: true,
+  });
+
+  console.log(response.data);
+}
+
 export async function createUser(email: string, password: string) {
-  try {
-    const response = await axios.post(
-      `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${API_KEY}`,
-      {
-        email: email,
-        password: password,
-        returnSecureToken: true,
-      }
-    );
-  } catch (error: unknown) {
-    if (axios.isAxiosError(error) && error.response) {
-      console.error("Error data:", error.response.data);
-    } else {
-      console.error(
-        "Error message:",
-        error instanceof Error ? error.message : String(error)
-      );
-    }
-  }
+  await authenticate("signUp", email, password);
+}
+
+export async function login(email: string, password: string) {
+  await authenticate("signInWithPassword", email, password);
 }
